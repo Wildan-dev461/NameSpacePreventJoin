62466af82cb2db25a92c3b057106e3dc5143c44b
will <112930040+Wildan-dev461@users.noreply.github.com>
2023-08-03 05:41:29 +0700

    Update NameSpacePreventJoin.php

---
+++ src/Wildan-Dev461/NameSpacePreventJoin/NameSpacePreventJoin.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wildan-Dev461\NameSpacePreventJoin;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class NameSpacePreventJoin extends PluginBase implements Listener {
+
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $event): void {
+        $player = $event->getPlayer();
+        $playerName = $player->getName();
+
+        if (strpos($playerName, " ") !== false) {
+            $player->kick(TextFormat::RED . "Namamu mengandung spasi harap diganti untuk memasuki server, terima kasih");
+        }
+    }
+}
