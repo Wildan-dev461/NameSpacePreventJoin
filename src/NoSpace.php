d12da69552a58a484be36318f4bdc29dea162164
will <112930040+Wildan-dev461@users.noreply.github.com>
2023-08-11 08:55:20 +0700

    Add option to kick or replace the space name

--- src/NoSpace.php
+++ src/NoSpace.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace wildan\nospace;
@@ -9,23 +10,27 @@ use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 
-class NoSpace extends PluginBase implements Listener{
-    protected function onEnable() : void{
+class NoSpace extends PluginBase implements Listener {
+    protected function onEnable() : void {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->saveDefaultConfig();
     }
 
-    public function onPlayerLogin(PlayerLoginEvent $event) : void{
+    public function onPlayerLogin(PlayerLoginEvent $event) : void {
         $player = $event->getPlayer();
         $playerName = $player->getName();
 
-		if(str_contains($playerName, " ")) {
-			if($this->getConfig()->get("ban-mode", false) == true) {
-				$this->getServer()->getNameBans()->addBan($playerName, $this->getConfig()->get("ban-message", "Your name is contain space, you can't play on this server!"), null, $this->getName());
-				$player->kick("You are banned from this server. Reason: " . $this->getConfig()->get("ban-message", "Your name is contain space, you can't play on this server!"));
-			} else {
-            	$player->kick(TextFormat::colorize($this->getConfig()->get("kick-message", "&cYour name is contain space, please change it before login again!")));
-        	}
+        if (str_contains($playerName, " ")) {
+            if ($this->getConfig()->get("replace-spaces", true)) {
+                $newName = str_replace(" ", "_", $playerName);
+                $player->setName($newName);
+                $player->sendMessage(TextFormat::colorize("&eYour name contains spaces and has been replaced with underscores."));
+            } elseif ($this->getConfig()->get("ban-mode", false) == true) {
+                $this->getServer()->getNameBans()->addBan($playerName, $this->getConfig()->get("ban-message", "Your name contains spaces, you can't play on this server!"), null, $this->getName());
+                $player->kick("You are banned from this server. Reason: " . $this->getConfig()->get("ban-message", "Your name contains spaces, you can't play on this server!"));
+            } elseif ($this->getConfig()->get("kick-on-space", true) == true) {
+                $player->kick(TextFormat::colorize($this->getConfig()->get("kick-message", "&cYour name contains spaces, please change it before logging in again!")));
+            }
         }
     }
 }
